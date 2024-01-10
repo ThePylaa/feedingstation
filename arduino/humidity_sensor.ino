b700b152c56932bc354aefd2c52f44400a3fdef1
Brilmayer, Paul - TIN2021 <brilmayerp.tin21@student.dhbw-heidenheim.de>
2024-01-10 13:16:58 +0100

    added comment for weird libarary use

--- arduino/humidity_sensor.ino
+++ arduino/humidity_sensor.ino
@@ -1,19 +1,23 @@
 #include "DHT.h"
 
-#define DHTPIN 2
+//ATTENTION: PIN NUMBERs: D5 = 5, not 8 like in pinlayout
+#define DHTPIN 5
+///
+
+
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
-  
+
 void setup() {
   Serial.begin(9600);
-    
+
   dht.begin();
 }
-  
+
 void loop() {
-  Serial.println("Temperature = " + String(dht.readTemperature())+" °C");
-  Serial.println("Humidite = " + String(dht.readHumidity())+" %");
-  
+  Serial.println("Temperature = " + String(dht.readTemperature()) + " °C");
+  Serial.println("Humidite = " + String(dht.readHumidity()) + " %");
+
   delay(3000);
 }
