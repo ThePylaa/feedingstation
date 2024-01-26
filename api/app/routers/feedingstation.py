f06a4d2f564c37d13d707b2846cc525b258f1adb
Paul <paulbrilmayer@gmail.com>
2024-01-26 11:36:22 +0100

    code cleanup + init get container_foodlevel

--- api/app/routers/feedingstation.py
+++ api/app/routers/feedingstation.py
@@ -1,18 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from utils.db.db import get_db
-from utils.auth.auth import hashPassword, verify_password, create_access_token
 from schemas.feedingstation import *
 from models.feedingstationmodel import FeedingStation_Model
-from datetime import datetime
 from uuid import uuid4
 
 
-# This is the user router. It is used to get the information of the current user, to update the information of the current user, to register a new user and to sign in and out.
 router = APIRouter(tags=["feedingstation"],prefix="/feedingstation")
 
-@router.get("/all_stations")
+@router.get("/all")
 def get_all_stations(db: Session = Depends(get_db)):
     """Function that returns all feeediong stations"""
     stations = db.query(FeedingStation_Model).all()
@@ -20,8 +16,8 @@ def get_all_stations(db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="No feedingstations found, db empty")
     return stations
 
-@router.get("/get_station_uuid")
-def get_station_uuid(db: Session = Depends(get_db)):
+@router.get("/new_station_uuid")
+def get_new_station_uuid(db: Session = Depends(get_db)):
     """Function that returns a uuid which isnt used yet"""
     feedingstation_id = uuid4()
 
@@ -37,7 +33,7 @@ def register(feedingstation: createFeedingstation, db: Session = Depends(get_db)
 
     existing_station = db.query(FeedingStation_Model).filter(FeedingStation_Model.feedingstation_id == feedingstation.feedingstation_id).first()
     if existing_station:
-        raise HTTPException(status_code=406, detail="Feedingstation already registered")
+        raise HTTPException(status_code=406, detail="Feedingstation / UUID already registered")
     
     dbStation = FeedingStation_Model(feedingstation_id=feedingstation.feedingstation_id, name=feedingstation.name)
     db.add(dbStation)
@@ -55,3 +51,11 @@ def update_container_foodlevel(feedingstation: updateFeedingstation, db: Session
     db.commit()
     db.refresh(existing_station)
     return existing_station
+
+@router.get("/container_foodlevel")
+def get_container_foodlevel(feedingstation_id: str, db: Session = Depends(get_db)):
+    """Function to get the foodlevel of a feedingstation. The user has to provide the feedingstation_id. The foodlevel is returned"""
+    existing_station = db.query(FeedingStation_Model).filter(FeedingStation_Model.feedingstation_id == feedingstation_id).first()
+    if not existing_station:
+        raise HTTPException(status_code=404, detail="Feedingstation not registered")
+    return existing_station.container_foodlevel
