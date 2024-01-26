ca082f8bfa51f08dba044e607631d277870ed2d0
Paul <paulbrilmayer@gmail.com>
2024-01-26 11:35:31 +0100

    changed db struct -> applied here too

--- api/app/models/animalmodel.py
+++ api/app/models/animalmodel.py
@@ -4,7 +4,7 @@ from utils.db.db import Base
 class Animal_Model(Base):
     __tablename__ = "animal"
 
-    animal_id = Column(Uuid(as_uuid=True), primary_key=True, index=True)
+    animal_rfid_id = Column(String, primary_key=True, index=True)
     user_id = Column(Uuid(as_uuid=True), ForeignKey("user.user_id") ,index=True, nullable=False)
     name = Column(String, index=True, nullable=False)
     type = Column(String, index=True, nullable=False)
--- api/app/models/portionmodel.py
+++ api/app/models/portionmodel.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, Uuid, String, ForeignKey, DateTime
+from sqlalchemy import Column, Uuid, String, ForeignKey, Time
 from utils.db.db import Base
 
 class Portion_Model(Base):
     __tablename__ = "portion"
 
     portion_id = Column(Uuid(as_uuid=True), primary_key=True, index=True)
-    accepted_animals_id = Column(Uuid(as_uuid=True), ForeignKey("accepted_animals.accepted_animals_id") ,index=True, nullable=False)
-    time = Column(DateTime, index=True, nullable=False)
+    time = Column(Time, index=True, nullable=False)
     size = Column(String, index=True, nullable=False)
+    feedingstation_id = Column(Uuid(as_uuid=True), ForeignKey("feedingstation.feedingstation_id"), nullable=False)
+    animal_rfid_id = Column(String, ForeignKey("animal.animal_rfid_id"), nullable=False)
     
