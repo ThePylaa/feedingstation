cb8737828ea4cb282669f8d9ec3a56abec3dea65
Brilmayer, Paul - TIN2021 <brilmayerp.tin21@student.dhbw-heidenheim.de>
2024-02-16 11:12:35 +0100

    added error handling and adjusted return latest picture

--- api/app/routers/picture_log.py
+++ api/app/routers/picture_log.py
@@ -5,6 +5,8 @@ from schemas.picture_log import *
 from models.picture_logmodel import Picture_Log_Model
 from uuid import uuid4
 from datetime import datetime, timedelta
+from models.usermodel import User_Model
+from models.feedingstationmodel import FeedingStation_Model
 
 
 router = APIRouter(tags=["picture"],prefix="/picture")
@@ -13,56 +15,94 @@ router = APIRouter(tags=["picture"],prefix="/picture")
 def get_all_pictures_by_userid(user_id: str, db: Session = Depends(get_db)):
     """Function that returns all pictures of a user"""
     delete_expired_pictures(db)
-    
-    pictures = db.query(Picture_Log_Model).filter(Picture_Log_Model.user_id == user_id).all()
-    if not pictures:
-        raise HTTPException(status_code=404, detail="No pictures found")
-    return pictures
 
-@router.get("/last_picture_by_userid")
+    try:
+        pictures = db.query(Picture_Log_Model).filter(Picture_Log_Model.user_id == user_id).all()
+        
+        if not pictures:
+            raise HTTPException(status_code=404, detail="No pictures found")
+
+        return pictures
+    
+    except:
+        raise HTTPException(status_code=404, detail="User not found")
+
+@router.get("/latest_picture_by_userid")
 def get_last_picture_by_userid(user_id: str, db: Session = Depends(get_db)):
-    """Function that returns the last picture of a user"""
+    """Function that returns the latest picture of a user"""
     delete_expired_pictures(db)
 
-    picture = db.query(Picture_Log_Model).filter(Picture_Log_Model.user_id == user_id).order_by(Picture_Log_Model.creation_date.desc()).first()
-    if not picture:
-        raise HTTPException(status_code=404, detail="No pictures found")
-    return picture
+    try:
+        picture = db.query(Picture_Log_Model).filter(Picture_Log_Model.user_id == user_id).order_by(Picture_Log_Model.creation_date.desc()).first()
+
+        if not picture:
+            raise HTTPException(status_code=404, detail="No picture found")
+
+        return picture
+
+    except:
+        raise HTTPException(status_code=404, detail="User not found")
 
 @router.get("/all_pictures_by_feedingstationid")
 def get_all_pictures_by_feedingstationid(feedingstation_id: str, db: Session = Depends(get_db)):
     """Function that returns all pictures of a feedingstation"""
     delete_expired_pictures(db)
 
-    pictures = db.query(Picture_Log_Model).filter(Picture_Log_Model.feedingstation_id == feedingstation_id).all()
-    if not pictures:
-        raise HTTPException(status_code=404, detail="No pictures found")
-    return pictures
+    try:
+        pictures = db.query(Picture_Log_Model).filter(Picture_Log_Model.feedingstation_id == feedingstation_id).all()
 
-@router.get("/last_picture_by_feedingstationid")
+        if not pictures:
+            raise HTTPException(status_code=404, detail="No pictures found")
+        
+        return pictures
+    
+    except:
+        raise HTTPException(status_code=404, detail="Feedingstation not found")
+
+
+@router.get("/latest_picture_by_feedingstationid")
 def get_last_picture_by_feedingstationid(feedingstation_id: str, db: Session = Depends(get_db)):
-    """Function that returns the last picture of a feedingstation"""
+    """Function that returns the latest picture of a feedingstation"""
     delete_expired_pictures(db)
 
-    picture = db.query(Picture_Log_Model).filter(Picture_Log_Model.feedingstation_id == feedingstation_id).order_by(Picture_Log_Model.creation_date.desc()).first()
-    if not picture:
-        raise HTTPException(status_code=404, detail="No pictures found")
-    return picture
+    try: 
+        picture = db.query(Picture_Log_Model).filter(Picture_Log_Model.feedingstation_id == feedingstation_id).order_by(Picture_Log_Model.creation_date.desc()).first()
+
+        if not picture:
+            raise HTTPException(status_code=404, detail="No picture found")
+        
+        return picture
+    
+    except:
+        raise HTTPException(status_code=404, detail="Feedingstation not found")
 
 @router.post("/upload_picture")
 def upload_picture(picture: uploadPicture, db: Session = Depends(get_db)):
     '''Function to upload a picture. The user has to provide the picture_id, the user_id, the feedingstation_id, the picture and the creation_date. The picture is stored in the database'''
     delete_expired_pictures(db)
 
-    picture_id = uuid4()
-    while db.query(Picture_Log_Model).filter(Picture_Log_Model.picture_id == picture_id).first():
-        picture_id = uuid4()
+    try: 
+        user = db.query(User_Model).filter(User_Model.user_id == picture.user_id).first()
+        feedingstation = db.query(FeedingStation_Model).filter(FeedingStation_Model.feedingstation_id == picture.feedingstation_id).first()
 
-    dbPicture = Picture_Log_Model(picture_id=picture_id, user_id=picture.user_id, feedingstation_id=picture.feedingstation_id, picture=picture.picture, creation_date=picture.creation_date)
-    db.add(dbPicture)
-    db.commit()
-    db.refresh(dbPicture)
-    return dbPicture
+        if not user:
+            raise HTTPException(status_code=404, detail="User not registered")
+        
+        if not feedingstation:
+            raise HTTPException(status_code=404, detail="Feedingstation not registered")
+        
+        picture_id = uuid4()
+        while db.query(Picture_Log_Model).filter(Picture_Log_Model.picture_id == picture_id).first():
+            picture_id = uuid4()
+
+        dbPicture = Picture_Log_Model(picture_id=picture_id, user_id=picture.user_id, feedingstation_id=picture.feedingstation_id, picture=picture.picture, creation_date=picture.creation_date)
+        db.add(dbPicture)
+        db.commit()
+        db.refresh(dbPicture)
+        return dbPicture
+    
+    except:
+        raise HTTPException(status_code=404, detail="User or feedingstation not found, mabye false Input (f.e. no UUID format given)")
 
 @router.delete("/delete_expired_pictures")
 def delete_expired_pictures(db: Session = Depends(get_db)):
