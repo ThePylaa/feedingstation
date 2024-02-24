7d9865451d9d236e42ec92ce8694782aaa815274
Pylaa <paulbrilmayer@gmail.com>
2024-02-24 15:59:17 +0100

    fixed bugs(mostly crashing when no wifi), added disconnect from wifi, beautification

--- raspberrypi/ui/wifiSetup.py
+++ raspberrypi/ui/wifiSetup.py
@@ -3,6 +3,7 @@ from tkinter import *
 import subprocess
 import os
 from raspberryUtils import hasInternet
+import time
 
 
 class WifiSetup(tk.Frame):
@@ -11,7 +12,12 @@ class WifiSetup(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-        available_networks = self.scan_wifi()
+        #if already connected to a network, dont scan
+        if hasInternet():
+            available_networks = ["Already connected to a", "network, press", " 'Disconnect from current", "Wifi' and refresh to", "change Wifi"]
+
+        else:
+            available_networks = self.scan_wifi()
 
         can = Canvas(self, height=480, width=800)
         can.place(relx=0.5, rely=0.5, anchor=CENTER)
@@ -43,9 +49,15 @@ class WifiSetup(tk.Frame):
         def get_selected_network():
             index = self.network_list.index(ACTIVE)
             if index != -1:  # Check if a selection exists
+                for i in range(self.network_list.size()):
+                    self.network_list.itemconfig(i, {'bg': 'white'})
+                self.network_list.itemconfig(index, {'bg': 'light blue'})
                 return self.network_list.get(index)
             else:
                 print("No network selected.")
+        
+        # Shows selected network in the listbox on FocusOut of Listbox
+        self.network_list.bind("<FocusOut>", lambda event: get_selected_network())
 
         # button to update the list of available networks
         global refreshImage
@@ -61,23 +73,42 @@ class WifiSetup(tk.Frame):
         password_entry.bind("<FocusIn>", lambda event: controller.show_keyboard())
         password_entry.bind("<FocusOut>", lambda event: controller.hide_keyboard())
 
+
         # button to connect to the selected network
-        connect_button = tk.Button(can, text="Connect", command=lambda: self.connect_to_wifi(get_selected_network(), self.password_var.get()), pady=10, background="grey", foreground="white", font=controller.main_font)
+        connect_button = tk.Button(can, text="Continue", command=lambda: self.connect_to_wifi(get_selected_network(), self.password_var.get()), pady=10, background="grey", foreground="white", font=controller.main_font)
         connect_button.pack()
+
+        # button to disconnect from the network
+        disconnect_button = tk.Button(can, text="Disconnect from current Wifi", command=lambda: self.disconnectFromWifi(), pady=10, background="grey", foreground="white", font=controller.main_font)
+        disconnect_button.pack()
 
         #debug button to show the next page, placed at the right top corner
         debug_button = tk.Button(self, text="Close", command=lambda: controller.destroy(), pady=10, background="grey", foreground="white", font=controller.main_font)
         debug_button.pack(side="right", anchor=NE)
 
     def scan_wifi(self):
-        networks = subprocess.check_output(["sudo", "iwlist", "wlan0", "scan"])
+        if hasInternet():
+            return ["Already connected to a", "network, press", " 'Disconnect from current", "Wifi' and refresh to", "change Wifi"]
+
+        for i in range (3):
+            try:
+                networks = subprocess.check_output(["iwlist", "wlan0", "scan"])
+                break
+            except:
+                print("Failed to scan networks")
+                time.sleep(1)
+                if i == 2:
+                    print("Failed to scan networks 3 times")
+                    return ["No networks found"]
         networks = networks.decode("utf-8") # converts the bytes to a string
         networks = networks.split("\n")     # splits the string into a list
         wifi_list = []
 
         for line in networks:
             if "ESSID" in line:
-                wifi_list.append(line.split('"')[1])
+                #check if SSID is already in wifi_list
+                if line.split('"')[1] not in wifi_list:
+                    wifi_list.append(line.split('"')[1])
                 
         return wifi_list
 
@@ -98,6 +129,12 @@ class WifiSetup(tk.Frame):
         if not ssid:
             print("Please select a network")
             return
+        if not password:
+            print("Please enter a password")
+            return
+        if ssid == "Already connected to a" or ssid == "network, press" or ssid == " 'Disconnect from current" or ssid == "Wifi' and refresh to" or ssid == "change Wifi":
+            print("Already connected to a network")
+            return
         #try 3 times to connect to the network
         for i in range(3):
             os.system('sudo raspi-config nonint do_wifi_ssid_passphrase "' + ssid + '" ' + password )
@@ -111,3 +148,11 @@ class WifiSetup(tk.Frame):
         else:
             print("Failed to connect to the network")
             self.controller.show_frame("WifiSetupErrorPage")
+
+    def disconnectFromWifi(self):
+        #get currnent ssid
+        ssid = subprocess.check_output("iwgetid -r", shell=True).decode("utf-8").strip()
+        print(ssid)
+        subprocess.run(["nmcli", "connection", "delete", ssid])
+        print("Disconnected from the network")
+
