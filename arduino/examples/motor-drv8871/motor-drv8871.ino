c5fc3364054328289328cbbd79ac91df56330889
Paul <97484777+ThePylaa@users.noreply.github.com>
2024-03-19 10:56:40 +0100

    magnet switch implemented

--- arduino/examples/motor-drv8871/motor-drv8871.ino
+++ arduino/examples/motor-drv8871/motor-drv8871.ino
@@ -1,4 +1,4 @@
-// Basic sketch for trying out the Adafruit DRV8871 Breakout
+#define MAGNETSWITCHPIN 35
 
 #define MOTOR_IN1 22
 
@@ -8,6 +8,7 @@ void setup() {
   Serial.println("DRV8871 test");
   
   pinMode(MOTOR_IN1, OUTPUT);
+  pinMode(MAGNETSWITCHPIN, INPUT);
 }
 
 void loop() {
@@ -16,21 +17,44 @@ void loop() {
     Serial.read();
   }
 
-  // ramp up forward
-  Serial.println("Start");
-  for (int i=127; i<132; i++) {
-    analogWrite(MOTOR_IN1, i);
-    delay(6);
+  // go away from current magnet
+  while(digitalRead(MAGNETSWITCHPIN) == 0){
+    for (int i=128; i<130; i++) {
+      analogWrite(MOTOR_IN1, i);
+      delay(4);
+    }
+    digitalWrite(MOTOR_IN1, LOW);
+    delay(1000);
   }
-  
-
-  // forward full speed for one second
   digitalWrite(MOTOR_IN1, LOW);
+
+  delay(1000);
+  
+  //got to next magnet
+  while(digitalRead(MAGNETSWITCHPIN) == 1){
+    for (int i=128; i<130; i++) {
+      analogWrite(MOTOR_IN1, i);
+      delay(4);
+    }
+    digitalWrite(MOTOR_IN1, LOW);
+    delay(1000);
+  }
+  digitalWrite(MOTOR_IN1, LOW);
+  
   delay(1000);
   byte n = Serial.available(); //check if charctaer(s)has been accumulated in buffer
-  /*while ( n == 0){
+  while ( n == 0){
     Serial.println("Wait for input");
     delay(100);
     n = Serial.available();
-  }*/
+  }
+  serialFlush();
+  Serial.println("Next round");
+
+}
+
+void serialFlush(){
+  while(Serial.available() > 0) {
+    char t = Serial.read();
+  }
 }
