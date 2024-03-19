e4ce5cd17a1caf2b2c1c9315108608d1ac7570a5
Paul <paulbrilmayer@gmail.com>
2024-03-19 09:17:55 +0100

    updated

--- arduino/examples/light_barrier/light_barrier.ino
+++ arduino/examples/light_barrier/light_barrier.ino
@@ -1,13 +1,13 @@
 #include <SharpDistSensor.h>
 
 // Analog pin to which the sensor is connected
-const byte sensorPin = A0;
+#define LIGHTBARRIERPIN A0
 
 // Window size of the median filter (odd number, 1 = no filtering)
-const byte medianFilterWindowSize = 5;
+#define MEDIANFILTERWINDOWSIZE 5
 
 // Create an object instance of the SharpDistSensor class
-SharpDistSensor sensor(sensorPin, medianFilterWindowSize);
+SharpDistSensor sensor(LIGHTBARRIERPIN, MEDIANFILTERWINDOWSIZE);
 
 void setup() {
   Serial.begin(9600);
@@ -23,6 +23,10 @@ void loop() {
   // Print distance to Serial
   Serial.println(distance);
 
+  if(distance < 200){
+    Serial.println("Close");
+  }
+
   // Wait some time
-  delay(50);
+  delay(200);
 }
